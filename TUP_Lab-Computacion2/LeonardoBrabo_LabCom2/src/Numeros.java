43aea125ae05e4d9cb6966613bff62f6fc50846b
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2023-08-11 11:50:29 -0300

    Ejercicio de ordenar 3 numeros de mayor a menor

---
+++ TUP_Lab-Computacion2/LeonardoBrabo_LabCom2/src/Numeros.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Numeros {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Ingrese tres numeros:");
+        System.out.println( "Primer numero:");
+        a = entrada.nextInt();
+
+        System.out.println("Segundo numero: ");
+        b = entrada.nextInt();
+
+        System.out.println("Tercer numero:");
+        c = entrada.nextInt();
+
+        if(a>b && b>c)
+            System.out.println( a+", "+b+", "+c);
+        else{
+            if(a>c && c>b)
+                System.out.println(a+", "+c+", "+b);
+            else{
+                if(b>a && a>c)
+                    System.out.println(b+", "+a+", "+c);
+                else{
+                    if(b>c && c>a)
+                        System.out.println(b+", "+c+", "+a);
+                    else{
+                        if(c>a && a>b)
+                            System.out.println(c+", "+a+", "+b);
+                        else{
+                            if(c>b && b>a)
+                                System.out.println(c+", "+b+", "+a);
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
